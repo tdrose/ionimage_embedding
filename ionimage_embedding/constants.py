b64293c23cea65f59d8b3a3e9fb4a655616edbca
Tim Daniel Rose <tim@rose-4-you.de>
2024-02-19 15:44:41 +0100

    Moved constants to top level.

---
+++ ionimage_embedding/constants.py
@@ -0,0 +1,24 @@
+from typing import Final, Dict
+
+# Constants for caching of data
+ION_IMAGE_DATA : Final[str] = 'IonImageData'
+DATASET_DATA : Final[str] = 'DatasetData'
+COLOC_NET_DISCRETE_DATA : Final[str] = 'ColocNetDiscreteData'
+
+# Data caching folder
+CACHE_FOLDER : Final[str] = '/scratch/model_testing'
+
+
+# Model show names:
+MEAN_COLOC : Final[str] = 'Mean Coloc'
+UMAP : Final[str] = 'UMAP'
+GNN : Final[str] = 'GNN'
+RANDOM : Final[str] = 'Random'
+
+# Colors
+MODEL_PALLETE : Final[Dict[str, str]] = {
+    MEAN_COLOC: 'darkgrey',
+    UMAP: 'gray',
+    GNN: 'red',
+    RANDOM: 'white'
+}
--- ionimage_embedding/dataloader/constants.py
+++
@@ -1,9 +0,0 @@
-from typing import Final
-
-# Constants for caching of data
-ION_IMAGE_DATA : Final = 'IonImageData'
-DATASET_DATA : Final = 'DatasetData'
-COLOC_NET_DISCRETE_DATA : Final = 'ColocNetDiscreteData'
-
-# Data caching folder
-CACHE_FOLDER : Final = '/scratch/model_testing'
